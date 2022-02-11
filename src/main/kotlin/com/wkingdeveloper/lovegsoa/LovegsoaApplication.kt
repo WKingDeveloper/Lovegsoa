174911ed980e589f1bb294c35ac2c4f856541902
WKingDeveloper <wkingdeveloper@gmail.com>
2022-02-11 19:43:13 +0900

    Initial commit

---
+++ src/main/kotlin/com/wkingdeveloper/lovegsoa/LovegsoaApplication.kt
@@ -0,0 +1,11 @@
+package com.wkingdeveloper.lovegsoa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LovegsoaApplication
+
+fun main(args: Array<String>) {
+    runApplication<LovegsoaApplication>(*args)
+}
